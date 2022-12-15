f5b7ecccd6c4707253654703c8bdb00f1d2c4600
AlexandreSz <75409769+AlexandreSz@users.noreply.github.com>
2022-12-15 10:38:47 +0100

    Add files via upload

--- scripts/factories/dropdown.js
+++ scripts/factories/dropdown.js
@@ -1,9 +1,13 @@
 let showSelectedTagsinHtml = document.getElementById("showSelectedTags");
 
-//dropdownMenu all link
+//dropdownMenu elements
 const dropdownMenuLinkBlue = document.getElementById("dropdownMenuLinkBlue");
 const dropdownMenuLinkGreen = document.getElementById("dropdownMenuLinkGreen");
 const dropdownMenuLinkRed = document.getElementById("dropdownMenuLinkRed");
+//dropdownContainer
+const dropdownContainerBlue = document.getElementById("filter_blue");
+const dropdownContainerGreen = document.getElementById("filter_green");
+const dropdownContainerRed = document.getElementById("filter_red");
 
 //ingredients
 function displayIngredientsTagList(ingredients) {
@@ -35,11 +39,16 @@ function displayIngredientsTagList(ingredients) {
 
 
 //onclick replace text
-dropdownMenuLinkBlue.addEventListener('click', function(event) {
-    event.target.parentNode.classList.add('filter_select_research_blue');
+dropdownContainerBlue.addEventListener('click', function(event) {
+    if (dropdownMenuLinkBlue.placeholder != "Ingredients") {
+        textModifblue()
+        return
+    }
+    event.target.classList.add('filter_select_research_blue');
     dropdownMenuLinkBlue.placeholder = 'Rechercher un ingrédient';
     const rafterRotation = document.getElementById('chevron_blue');
     rafterRotation.classList.add('fas-solid_rotate');
+
 })
 
 //replacer le dropdown ingredient
@@ -49,6 +58,7 @@ function textModifblue() {
     Style.classList.remove('filter_select_research_blue');
     const rafterRotation = document.getElementById('chevron_blue');
     rafterRotation.classList.remove('fas-solid_rotate');
+
 }
 
 //input filter for ingredients
@@ -94,17 +104,21 @@ function displayApplianceTagList(appliance) {
     })
 }
 //onclick replace text
-dropdownMenuLinkGreen.addEventListener('click', function(event) {
+dropdownContainerGreen.addEventListener('click', function(event) {
+    if (dropdownMenuLinkGreen.placeholder != "Appareils") {
+        textModifgreen()
+        return
+    }
     event.target.parentNode.classList.add('filter_select_research_green');
     dropdownMenuLinkGreen.placeholder = 'Rechercher un appareil';
-
     const rafterRotation = document.getElementById('chevron_green');
     rafterRotation.classList.add('fas-solid_rotate');
+
 })
 
 //replacer le dropdown appareilst
 function textModifgreen() {
-    dropdownMenuLinkGreen.placeholder = "appareils";
+    dropdownMenuLinkGreen.placeholder = "Appareils";
     const Style = document.getElementById("filter_green");
     Style.classList.remove('filter_select_research_green');
     const rafterRotation = document.getElementById('chevron_green');
@@ -147,9 +161,13 @@ function displayUstensilsTagList(ustensils) {
 }
 
 //onclick replace text
-dropdownMenuLinkRed.addEventListener('click', function(event) {
+dropdownContainerRed.addEventListener('click', function(event) {
+        if (dropdownMenuLinkRed.placeholder != "Ustensiles") {
+            textModifred()
+            return
+        }
         event.target.parentNode.classList.add('filter_select_research_red');
-        dropdownMenuLinkRed.placeholder = 'Rechercher un ustensile';
+        dropdownMenuLinkRed.placeholder = 'Rechercher un ustensil';
         dropdownMenuLinkRed.classList.add('col-xs-4');;
 
         const rafterRotation = document.getElementById('chevron_red');
@@ -157,7 +175,7 @@ dropdownMenuLinkRed.addEventListener('click', function(event) {
     })
     //replacer le dropdown appareilst
 function textModifred() {
-    dropdownMenuLinkRed.placeholder = "ustensils";
+    dropdownMenuLinkRed.placeholder = "Ustensiles";
     const Style = document.getElementById("filter_red");
     Style.classList.remove('filter_select_research_red');
     const rafterRotation = document.getElementById('chevron_red');
